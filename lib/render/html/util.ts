07752315d69ab94da99253ede07c2abe3bf38af6
LoveKicher <szlproche@163.com>
2024-06-22 22:08:03 +0800

    fix: 自己绘图，尝试终结字体渲染问题

--- lib/render/html/util.ts
+++ lib/render/html/util.ts
@@ -3,8 +3,34 @@ import { DrawableElement } from "../../meta/element";
 import { getUnitValue } from "@lib/math/UnitValue";
 import { Vector2 } from "@lib/math";
 // import html2canvas from "html2canvas";
-import { domToBlob, Options } from "modern-screenshot";
+import {
+  domToForeignObjectSvg,
+  Options,
+  waitUntilLoad,
+} from "modern-screenshot";
 import { ExportImageOption } from "@lib/index";
+
+/**
+ * from `modern-screenshot/src/utils.ts`
+ */
+function svgToDataUrl(svg: SVGElement, removeControlCharacter = true) {
+  let xhtml = new XMLSerializer().serializeToString(svg);
+  if (removeControlCharacter) {
+    xhtml = xhtml.replace(
+      /[\u0000-\u0008\u000B\u000C\u000E-\u001F\uD800-\uDFFF\uFFFE\uFFFF]/gu,
+      ""
+    );
+  }
+  return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(xhtml)}`;
+}
+
+async function setImmediate() {
+  await new Promise<void>((s) => {
+    setTimeout(() => {
+      s();
+    }, 10);
+  });
+}
 
 export function getLayout(e: DrawableElement) {
   let origin = e.origin;
@@ -20,10 +46,9 @@ export function getLayout(e: DrawableElement) {
   };
 
   let mode = e.layoutMode;
-  attrs.position = mode  || "static";
+  attrs.position = mode || "static";
   return attrs;
 }
-
 
 // export async function renderToImageBad(root: HTMLElement) {
 //   const canvas = await html2canvas(root, {
@@ -41,7 +66,7 @@ export function getLayout(e: DrawableElement) {
 //       });
 //     } catch (error) {
 //       e();
-//     }  
+//     }
 //   })
 // }
 
@@ -68,7 +93,7 @@ function getMaxRect(root: HTMLElement) {
     top: parentRect.top,
     right: parentRect.right,
     bottom: parentRect.bottom,
-    left: parentRect.left
+    left: parentRect.left,
   };
 
   // 遍历子元素，计算占据的空间
@@ -84,23 +109,25 @@ function getMaxRect(root: HTMLElement) {
   let parentWidth = parentSpace.right - parentSpace.left;
   let parentHeight = parentSpace.bottom - parentSpace.top;
 
-
   // 把相对window的偏移转换成相对父元素的偏移
   return new DOMRect(
-    parentSpace.left - parentRect.left, 
-    parentSpace.top - parentRect.top, 
-    parentWidth, 
-    parentHeight,
+    parentSpace.left - parentRect.left,
+    parentSpace.top - parentRect.top,
+    parentWidth,
+    parentHeight
   );
 }
 
-export async function renderToImage(root: HTMLElement, option: ExportImageOption = {}) {
+export async function renderToImage(
+  root: HTMLElement,
+  option: ExportImageOption = {}
+) {
   let opt: Options = {};
   opt.backgroundColor = option.backgroundColor || "transparent";
   if (option.font) {
     opt.font = {
-      cssText: option.font
-    }
+      cssText: option.font,
+    };
   }
 
   opt.type = option.imageFormat;
@@ -115,9 +142,32 @@ export async function renderToImage(root: HTMLElement, option: ExportImageOption
     opt.style.marginLeft = -rect.left + "px";
     opt.style.marginTop = -rect.top + "px";
 
-    console.log(opt, rect)
+    // console.log(opt, rect);
   }
-  
 
-  return await domToBlob(root, opt);
+  // 不使用modern-screenshot的domToBlob方法，因为该方法不会等待SVG生成的img加载完
+  const svg = await domToForeignObjectSvg(root, opt);
+  const img = new Image();
+  img.src = svgToDataUrl(svg, true);
+
+  console.time("load img");
+  await waitUntilLoad(img, 5000);
+  console.timeEnd("load img");
+
+  // 画图是异步的，等待一个微小间隔，否则部分字体加载不出来
+  await setImmediate();
+
+  const canvas = document.createElement("canvas");
+  canvas.width = opt.width;
+  canvas.height = opt.height;
+  const ctx = canvas.getContext("2d")!;
+  ctx.fillStyle = "transparent";
+  ctx.fillRect(0, 0, opt.width, opt.height);
+  ctx.drawImage(img, 0, 0, opt.width, opt.height);
+
+  return await new Promise((s, e) => {
+    canvas.toBlob((blob) => {
+      blob ? s(blob) : e();
+    }, opt.type || "image/png");
+  });
 }
